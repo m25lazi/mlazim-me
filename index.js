2005c137fa8cd752cb19422370f9609256e0e23c
m25lazi <m25lazi@gmail.com>
2015-11-22 18:55:48 +0530

    lab init test

--- index.js
+++ index.js
@@ -24,6 +24,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use(express.static(__dirname + '/public'));
+app.use('/bower_components',express.static(__dirname + '/bower_components'));
 
 app.use(function(req, res, next){
     res.setHeader('Access-Control-Allow-Origin', '*');
