083738ec99c2ff56c2619542c922a0c6cb05003c
m25lazi <m25lazi@gmail.com>
2015-11-22 18:41:08 +0530

    init

---
+++ index.js
@@ -0,0 +1,31 @@
+var path = require('path');
+global.appRoot = path.resolve(__dirname);
+
+
+var express = require('express');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var firebase = require("firebase");
+
+var app = express();
+var port_number = process.env.PORT || 3000;
+var server = app.listen(port_number, function () {
+
+  var host = server.address().address;
+  var port = server.address().port;
+
+  console.log("API app listening at http://%s:%s", host, port);
+  
+});
+
+
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(function(req, res, next){
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    return next();
+});
