183be69492b49ec8351fe3101dab7628957139bf
m25lazi <m25lazi@gmail.com>
2016-01-01 22:17:14 +0530

    CORS header security
    Resolving “Request header field content-type is not allowed by
    Access-Control-Allow-Headers in preflight response.”

--- arduino.js
+++ arduino.js
@@ -34,6 +34,14 @@ app.use(function(req, res, next){
     return next();
 });
 
+// CORS header securiy
+app.all('/*', function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    next();
+});
+
 
 app.get('/testpost', function (req, res) {
     console.log("GOT GET REQUEST FROM ARDUINO");
